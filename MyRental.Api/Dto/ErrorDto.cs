c2e3365c9a9c2982805dcf6906aec78469a07a3d
Vladislawv <87234450+Vladislawv@users.noreply.github.com>
2022-08-18 21:41:48 +0300

    Add exception handler middleware (#3)
    feat - Added exception handler middleware;
    feat - Added ErrorDto;
    feat - Added HomeController with test method.

---
+++ MyRental.Api/Dto/ErrorDto.cs
@@ -0,0 +1,6 @@
+﻿namespace MyRental.Api.Dto;
+
+public class ErrorDto
+{
+    public string ErrorMessage { get; set; }
+}
---
+++ MyRental.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics;
+using MyRental.Api.Dto;
+
+namespace MyRental.Api.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(ex, context);
+        }
+    }
+
+    private async Task HandleExceptionAsync(Exception ex, HttpContext context)
+    {
+        _logger.LogError(ex.Message);
+        var response = context.Response;
+        
+        response.ContentType = "application/json";
+        response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+        ErrorDto errorDto = new()
+        {
+            ErrorMessage = ex.Message
+        };
+
+        var result = JsonSerializer.Serialize(errorDto, new JsonSerializerOptions(JsonSerializerDefaults.Web));
+
+        await response.WriteAsync(result);
+    }
+}
